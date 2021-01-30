6a009de1881ef044d278ace46948b6aab5ccf842
“Debora <debora.crescenzo@gmail.com>
2021-01-30 17:24:52 +0000

    test

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_ID,
+  apiKey: "AIzaSyB6LVr8aUUmXdQhlKYGlIVGluJHuX-xEm0",
+  authDomain: "blog-vue-ffd4e.firebaseapp.com",
+  projectId: "blog-vue-ffd4e",
+  storageBucket: "blog-vue-ffd4e.appspot.com",
+  messagingSenderId: "5454428996",
+  appId: "1:5454428996:web:dd19a74d47d35f22f03511",
 };
 
 // init firebase
