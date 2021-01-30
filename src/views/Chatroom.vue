69ecc83842f24ba0d67b4f4e1ef9c32c7cae113d
“Debora <debora.crescenzo@gmail.com>
2021-01-30 16:09:45 +0000

    auto-scroll

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="container">
-  <Navbar/>
-  <NewChatForm/>
+    <Navbar />
+    <ChatWindow />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
-import Navbar from "../components/Navbar"
-import NewChatForm from "./NewChatForm"
+import NewChatForm from '../components/NewChatForm.vue'
+import ChatWindow from '../components/ChatWindow.vue'
+import Navbar from '../components/Navbar.vue'
 import getUser from '../composables/getUser'
 import { watch } from 'vue'
 import { useRouter } from 'vue-router'
-
 export default {
-    components : { Navbar, NewChatForm },
-    setup (){
-        const { user } = getUser()
-
-        const router = useRouter()
-
-        watch (user, () => {
-              if(!user.value){
-                router.push({ name: 'Welcome' })
-              }
-        })
-
-
-
-        
-    }
+  components: { Navbar, NewChatForm, ChatWindow },
+  setup() {
+    const router = useRouter()
+    const { user } = getUser()
+    // if the user value is ever null, redirect to welcome screen
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: 'Welcome' })
+      }
+    })
+  }
 }
 </script>
 
 <style>
-
 </style>
